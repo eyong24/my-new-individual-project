47b80c76dd4e2bc0de3862a2b2f718373fcc7ea6
eyong24 <eyongtubao@gmail.com>
2023-03-14 16:38:13 +0800

    finish most of the work in individual project

--- javascript/placeholder.js
+++ javascript/placeholder.js
@@ -43,8 +43,67 @@ window.onscroll = () =>
     
 }
 
+
+// auto input
+
+let typed = new Typed ('.autoinput',{
+    strings:['MOTHERBOARD OF 2023.','CPU OF 2023.','GPU OF 2023.','MONITOR OF 2023.'],
+    typeSpeed:100,
+    backSpeed: 100,
+    backDelay:2000,
+    loop:true,
+}) 
+
 // let navbar = document.querySelector('.navbar');
 // document.querySelector('#menu-btn').onclick = () =>
 // {
 //     navbar.classList.toggle('active');
 // }
+
+// swiper js
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    slidesPerView: 3,
+    spaceBetween: 40,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true},
+
+breakpoints: {
+0:{
+    slidesPerView: 1,
+},
+768:{
+    slidesPerView: 2,
+},
+
+1024:{
+    slidesPerView: 3,
+},
+},
+
+});
+
+var swiper = new Swiper(".categories-slider", {
+    loop:false,
+    slidesPerView: 3,
+    spaceBetween: 40,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true},
+
+breakpoints: {
+0:{
+    slidesPerView: 1,
+},
+768:{
+    slidesPerView: 2,
+},
+
+1024:{
+    slidesPerView: 3,
+},
+},
+
+});
